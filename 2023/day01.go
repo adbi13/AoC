a0ad4b878c74d2771301e4658d9c793fa958b35a
Adela Bierska <adela.bierska@gmail.com>
2023-12-01 15:36:05 +0100

    Day 1: Trebuchet?! Part 2

--- 2023/day01.go
+++ 2023/day01.go
@@ -3,8 +3,47 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func processLine(line string) int {
+	numStrings := []string {
+		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		"1", "2", "3", "4", "5", "6", "7", "8", "9",
+	}
+
+	var firstNum int
+	var lastNum int
+	var minPosition = math.MaxInt
+	var maxPosition int = -1
+
+	for numIndex, numString := range numStrings {
+		firstIndex := strings.Index(line, numString)
+		if firstIndex != -1 && firstIndex < minPosition {
+			minPosition = firstIndex
+			if numIndex < 9 {
+				firstNum = numIndex + 1
+			} else {
+				firstNum, _ = strconv.Atoi(numString)
+			}
+		}
+
+		lastIndex := strings.LastIndex(line, numString)
+		if lastIndex != -1 && lastIndex > maxPosition {
+			maxPosition = lastIndex
+			if numIndex < 9 {
+				lastNum = numIndex + 1
+			} else {
+				lastNum, _ = strconv.Atoi(numString)
+			}
+		}
+	}
+
+	return firstNum * 10 + lastNum
+}
 
 func day01() {
 	file, err := os.Open("input01.txt")
@@ -17,19 +56,7 @@ func day01() {
 	scanner := bufio.NewScanner(file)
 	var result = 0
 	for scanner.Scan() {
-		var firstNum = -1
-		var lastNum = -1
-		line := scanner.Text()
-		for i := 0; i < len(line); i++ {
-			num := int(line[i] - '0')
-			if num >= 0 && num <= 9 {
-				if firstNum < 0 {
-					firstNum = num * 10
-				}
-				lastNum = num
-			}
-		}
-		result += firstNum + lastNum
+		result += processLine(scanner.Text())
 	}
 
 	fmt.Println(result)
